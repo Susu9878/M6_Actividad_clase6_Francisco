bf15cdfd9e08e489b11af1cd6ea86439db5741b7
Susu9878 <jalonzo9878@gmail.com>
2026-04-08 08:35:55 -0600

    initial project creation

---
+++ backend/src/test/java/com/backend/m6_activity/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.m6_activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
